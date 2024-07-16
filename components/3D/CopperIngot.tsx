9daf357ebf380f87cd4337eb2c7b1543be275357
tymyevhenmpw <139116341+tymyevhenmpw@users.noreply.github.com>
2024-07-16 21:50:29 +0300

    Commit

--- components/3D/CopperIngot.tsx
+++ components/3D/CopperIngot.tsx
@@ -5,18 +5,19 @@ import { useRef } from 'react';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 
-useGLTF.preload("/3D/Copper-ingot.glb");
 
 const CopperIngot = () => {
-    const modelRef = useRef<THREE.Group>(null);
-
-    const { nodes } = useGLTF("/3D/Copper-ingot.glb");
-
-    return (
-      <group ref={modelRef}>
+  const modelRef = useRef<THREE.Group>(null);
+  
+  const { nodes } = useGLTF("/3D/Copper-ingot.glb");
+  
+  return (
+    <group ref={modelRef}>
         <mesh {...nodes.mesh_0}></mesh>
       </group>
     );
-}
-
+  }
+  
 export default CopperIngot;
+
+useGLTF.preload("/3D/Copper-ingot.glb");
--- components/3D/CopperIngotPolished.tsx
+++ components/3D/CopperIngotPolished.tsx
@@ -5,20 +5,21 @@ import { useRef } from 'react';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 
-useGLTF.preload("/3D/Copper-ingot-polished.glb");
 
 const CopperIngotPolished = () => {
-    const modelRef = useRef<THREE.Group>(null);
-
-    const { nodes } = useGLTF("/3D/Copper-ingot-polished.glb");
-
-    console.log(nodes);
+  const modelRef = useRef<THREE.Group>(null);
   
-    return (
-      <group ref={modelRef} rotation={[0.2, 0, 0]}>
+  const { nodes } = useGLTF("/3D/Copper-ingot-polished.glb");
+  
+  console.log(nodes);
+  
+  return (
+    <group ref={modelRef} rotation={[0.2, 0, 0]}>
         <mesh {...nodes.pCube1_lambert1_0}  position={[0, -0.5, 0]}></mesh>
       </group>
     );
-}
-
+  }
+  
 export default CopperIngotPolished;
+  
+useGLTF.preload("/3D/Copper-ingot-polished.glb");
