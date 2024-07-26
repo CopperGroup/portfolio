cee7fb8c65985c6cad2ab64e90c8257d093e87cf
tymyevhenmpw <139116341+tymyevhenmpw@users.noreply.github.com>
2024-07-26 22:11:43 +0300

    Adapted Skils section

--- components/sections/Skils.tsx
+++ components/sections/Skils.tsx
@@ -35,7 +35,7 @@ const Skils = () => {
 
 
   return (
-    <section className="w-full h-[130vh] bg-white flex flex-1 flex-col justify-center items-center px-24 py-[5.7rem]">
+    <section className="w-full h-[130vh] bg-white flex flex-1 flex-col justify-center items-center px-24 py-[5.7rem] max-[1600px]:h-[145vh] max-[1440px]:h-fit max-[990px]:px-20 max-[820px]:px-16 max-[750px]:px-10 max-[670px]:px-7">
       <motion.div 
        ref={headerRef}
        className="w-full h-fit flex gap-2 justify-center items-center mb-20"
@@ -44,17 +44,17 @@ const Skils = () => {
         y: headerInView ? 0 : 50
        }}
       >
-        <FaStarOfLife className="size-20 gradient text-white rounded-full p-3"/>
-        <h2 className="relative text-[96px] font-bold text-center">
+        <FaStarOfLife className="size-20 gradient text-white rounded-full p-3 max-[920px]:size-16 max-[645px]:size-10 max-[645px]:p-2 max-[485px]:hidden"/>
+        <h2 className="relative text-[6rem] font-bold text-center max-[1040px]:text-[5rem] max-[925px]:text-[4rem] max-[645px]:text-[3rem] max-[390px]:text-[2.5rem] max-[335px]:text-[2.35rem]">
           Our techstack
         </h2>
-        <FaStarOfLife className="size-20 gradient text-white rounded-full p-3"/>
+        <FaStarOfLife className="size-20 gradient text-white rounded-full p-3 max-[920px]:size-16 max-[645px]:size-10 max-[645px]:p-2 max-[485px]:hidden"/>
       </motion.div>
       <div className="w-full h-full flex flex-col gap-7">
-        <div className="w-full h-3/4 flex flex-1 gap-7">
+        <div className="w-full h-3/4 flex flex-1 gap-7 max-[780px]:flex-col">
           <motion.div
            ref={cardsRef}
-           className="w-7/12 h-full px-5 border rounded-2xl shadow-xl"
+           className="w-7/12 h-full px-5 border rounded-2xl shadow-xl max-[1440px]:pt-[7%] max-[1440px]:pb-[3%] max-[780px]:w-full"
           //  onMouseMove={(e) => handleMouseMove(e)}
           //  onMouseLeave={() => handleMouseLeave()}
            style={{
@@ -68,17 +68,18 @@ const Skils = () => {
            }}
           >
             <motion.div 
-             className="relative w-full h-5/6 flex justify-center items-center" 
+             className="relative w-full h-5/6 flex justify-center items-center max-[1440px]:mb-[5%]" 
             >
               <SiNextdotjs className="size-56 rounded-full border shadow-xl"/> 
             </motion.div>
-            <div className="w-full h-1/6 text-black">
+            <div className="w-full h-1/6 text-black border">
               <h4 className="text-heading4-medium">Next.js</h4>
-              <p className="text-base-regular text-gray-500/90">One of the best frameworks for creating cutting-edge web-aplications.</p>
+              <p className="text-base-regular text-gray-500/90 max-[400px]:hidden">One of the best frameworks for creating cutting-edge web-applications.</p>
+              <p className="text-base-regular text-gray-500/90 min-[400px]:hidden">Framework for cutting-edge web-applications.</p>
             </div>
           </motion.div>
           <motion.div 
-           className="w-5/12 h-full px-5 border rounded-2xl shadow-xl"
+           className="w-5/12 h-full px-5 border rounded-2xl shadow-xl max-[1440px]:pt-[7%] max-[1440px]:pb-[3%] max-[780px]:w-full"
            whileHover={{
             scale: 1.01
            }}
@@ -90,24 +91,25 @@ const Skils = () => {
             duration: 0.5
            }}
           >
-            <div className="w-full h-5/6 flex justify-center items-center">
+            <div className="w-full h-5/6 flex justify-center items-center max-[1440px]:mb-[5%] max-[1213px]:mb-[12%] max-[1069px]:mb-[5%]">
               <SiReact className="size-52 rounded-full border text-black shadow-xl p-5"/> 
             </div>
             <div className="w-full h-1/6 text-black">
               <h4 className="text-heading4-medium">React</h4>
-              <p className="text-base-regular text-gray-500/90">The essential building block for achieving speed and quality on your website.</p>
+              <p className="text-base-regular text-gray-500/90 max-[400px]:hidden">The essential building block for achieving speed and quality on your website.</p>
+              <p className="text-base-regular text-gray-500/90 min-[400px]:hidden">The speed, quality, reliability for your website.</p>
             </div>
           </motion.div>
         </div>
-        <div className="w-full h-1/4 flex gap-7">
+        <div className="w-full h-1/4 flex gap-7 max-[1440px]:flex-col">
           <motion.div 
-           className="w-1/2 h-full flex gap-7"
+           className="w-1/2 h-full flex gap-7 max-[1440px]:w-full max-[520px]:flex-col"
            style={{
             x: leftOffset
            }}
           >
             <motion.div 
-             className="w-1/2 h-full px-5 border rounded-2xl shadow-xl"
+             className="w-1/2 h-full px-5 border rounded-2xl shadow-xl max-[1440px]:pt-[3%] max-[1440px]:pb-[1%] max-[520px]:w-full max-[520px]:pb-[3%]"
              whileHover={{
               scale: 1.01
              }}
@@ -116,7 +118,7 @@ const Skils = () => {
               duration: 0.5
              }}
             >
-                <div className="w-full h-2/3 flex justify-center items-center">
+                <div className="w-full h-2/3 flex justify-center items-center max-[1440px]:mb-[5%]">
                   <SiMongodb className="size-28 rounded-full border text-black shadow-xl p-5"/> 
                 </div>
                 <div className="w-full h-1/3 text-black">
@@ -125,7 +127,7 @@ const Skils = () => {
                 </div>
             </motion.div>
             <motion.div 
-             className="w-1/2 h-full px-5 border rounded-2xl shadow-xl"
+             className="w-1/2 h-full px-5 border rounded-2xl shadow-xl max-[1440px]:pt-[3%] max-[1440px]:pb-[1%] max-[520px]:w-full max-[520px]:pb-[3%]"
              whileHover={{
               scale: 1.01
              }}
@@ -134,7 +136,7 @@ const Skils = () => {
               duration: 0.5
              }}
             >
-              <div className="w-full h-2/3 flex justify-center items-center">
+              <div className="w-full h-2/3 flex justify-center items-center max-[1440px]:mb-[5%]">
                 <SiThreedotjs className="size-28 rounded-full border text-black shadow-xl p-5 pl-8 pt-6 overflow-visible"/> 
               </div>
               <div className="w-full h-1/3 text-black">
@@ -147,10 +149,10 @@ const Skils = () => {
            style={{
             x: rightOffset
            }} 
-           className="w-1/2 h-full flex gap-7"
+           className="w-1/2 h-full flex gap-7 max-[1440px]:w-full max-[520px]:flex-col"
           >
             <motion.div 
-             className="w-1/3 h-full px-5 border rounded-2xl shadow-xl"
+             className="w-1/3 h-full px-5 border rounded-2xl shadow-xl max-[1440px]:pt-[3%] max-[1440px]:pb-[1%] max-[520px]:w-full max-[520px]:pb-[3%]"
              whileHover={{
               scale: 1.01
              }}
@@ -159,18 +161,19 @@ const Skils = () => {
               duration: 0.5
              }}
             >
-              <div className="w-full h-2/3 flex justify-center items-center">
+              <div className="w-full h-2/3 flex justify-center items-center max-[1440px]:mb-[5%]">
                 <div className="w-fit h-fit rounded-full shadow-xl p-5">
                   <SiJavascript className="size-12 border text-black"/> 
                 </div>
               </div>
               <div className="w-full h-1/3 text-black">
                 <h4 className="text-body-medium">Javascript</h4>
-                <p className="text-small-regular text-gray-500/90">Our pragramming language.</p>
+                <p className="text-small-regular text-gray-500/90 max-[780px]:hidden">Programming language.</p>
+                <p className="text-small-regular text-gray-500/90 min-[520px]:hidden">Our programming language.</p>
               </div>
             </motion.div>
             <motion.div 
-             className="w-1/3 h-full px-5 border rounded-2xl shadow-xl"
+             className="w-1/3 h-full px-5 border rounded-2xl shadow-xl max-[1440px]:pt-[3%] max-[1440px]:pb-[1%] max-[520px]:w-full max-[520px]:pb-[3%]"
              whileHover={{
               scale: 1.01
              }}
@@ -179,16 +182,17 @@ const Skils = () => {
               duration: 0.5
              }}
             >
-              <div className="w-full h-2/3 flex justify-center items-center">
+              <div className="w-full h-2/3 flex justify-center items-center max-[1440px]:mb-[5%]">
                 <SiTailwindcss className="size-[5.5rem] rounded-full border text-black shadow-xl p-5"/> 
               </div>
               <div className="w-full h-1/3 text-black">
                 <h4 className="text-body-medium">Tailwindcss</h4>
-                <p className="text-small-regular text-gray-500/90">Style faster and efficientely.</p>
+                <p className="text-small-regular text-gray-500/90 max-[780px]:hidden">Style.</p>
+                <p className="text-small-regular text-gray-500/90 min-[520px]:hidden">Style efficiently and faster.</p>
               </div>
             </motion.div>
             <motion.div 
-             className="w-1/3 h-full px-5 border rounded-2xl shadow-xl"
+             className="w-1/3 h-full px-5 border rounded-2xl shadow-xl max-[1440px]:pt-[3%] max-[1440px]:pb-[1%] max-[520px]:w-full max-[520px]:pb-[3%]"
              whileHover={{
               scale: 1.01
              }}
@@ -197,12 +201,13 @@ const Skils = () => {
               duration: 0.5
              }}
             >
-              <div className="w-full h-2/3 flex justify-center items-center">
+              <div className="w-full h-2/3 flex justify-center items-center max-[1440px]:mb-[5%]">
                 <SiCodemagic className="size-[5.5rem] rounded-full border text-black shadow-xl p-5"/> 
               </div>
               <div className="w-full h-1/3 text-black">
                 <h4 className="text-body-medium">UI/UX</h4>
-                <p className="text-small-regular text-gray-500/90">Get yours modern design.</p>
+                <p className="text-small-regular text-gray-500/90 max-[780px]:hidden">Yours modern design.</p>
+                <p className="text-small-regular text-gray-500/90 min-[520px]:hidden">Best modern desing practices</p>
               </div>
             </motion.div>
           </motion.div>
